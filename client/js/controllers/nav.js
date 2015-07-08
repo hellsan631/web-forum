2bc48c47d57927db6402e2d7ad56312681874151
Mathew Kleppin <matkle414@gmail.com>
2015-07-07 21:17:47 -0500

    added login/logout/getCurrentUser

--- client/js/controllers/nav.js
+++ client/js/controllers/nav.js
@@ -1,11 +1,37 @@
 angular
   .module("web-forum")
-  .controller("NavController", ["$scope", "Person",
-    function($scope, Person){
+  .controller("NavController", ["$scope", "$rootScope", "Person", 'LoopBackAuth',
+    function($scope, $rootScope, Person, LoopBackAuth){
         $scope.name = "It Works!";
 
         $(".button-collapse").sideNav();
         $('.modal-trigger').leanModal();
+
+        getCurrentUser();
+
+        $scope.logout = function(){
+          Person.logout();
+          $rootScope.currentUser = null;
+        };
+
+        $scope.openLoginModal = function(){
+          $('.button-collapse').sideNav('hide');
+
+          $('#LoginModal').openModal();
+
+          $scope.submitLogin = function(){
+            Person.login($scope.login, function(response){
+
+              LoopBackAuth.currentUserId = response.userId;
+              LoopBackAuth.accessTokenId = response.id;
+              LoopBackAuth.save();
+
+              $rootScope.currentUser = response.user;
+
+              $('#LoginModal').closeModal();
+            });
+          };
+        };
 
         $scope.openRegisterModal = function(){
           $('.button-collapse').sideNav('hide');
@@ -30,16 +56,30 @@ angular
 
             Person.create(newPerson,
               function(response){
-                Materialize.toast('Success!!', 4000);
+                Materialize.toast('Success!! Logging you in.', 4000);
 
                 Person.login(loginFields, function(response){
-                  console.log(response);
+                  LoopBackAuth.currentUserId = response.userId;
+                  LoopBackAuth.accessTokenId = response.id;
+                  LoopBackAuth.save();
                 });
+
+                $('#RegisterModal').closeModal();
               },
               function(error){
                 Materialize.toast('You\'ve already registered dummy', 4000);
               });
           };
         };
+
+        function getCurrentUser(){
+          Person.getCurrent(function(response){
+              $rootScope.currentUser = response;
+            },
+            function(error){
+              if(error.status !== 401)
+                Person.logout();
+            });
+        }
 
     }]);
